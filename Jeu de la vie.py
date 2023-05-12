743cf3fd7a67a9f65f73e26e1354ec7bc87b4824
BUSTOS THIBAULT <thibault.bustos1234@gmail.com>
2023-05-12 13:45:19 +0200

    Update Jeu de la vie.py

--- Jeu de la vie.py
+++ Jeu de la vie.py
@@ -4,58 +4,66 @@ from random import randint
 
 
 
-def creerTNull(nbL, nbC):
-    T = [[0 for i in range(nbC)] for j in range(nbL)]
-    for i in range(1, len(T)-1):
-        for j in range(1, len(T[i])-1):
-            T[i][j] = randint(0, 1)
-    return T
+def init_matrix(rows_count, columns_count):
+    return [ [ randint(0, 1)
+            for x in range(columns_count) ] 
+            for y in range(rows_count) ]
 
 
 
-def nbVoisins(T, l, c):
-    nb = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            nb += T[l+i][c+j]
-    return nb - T[l][c]
+def count_neighbors(matrix, y, x):
+    rows_count = len(matrix)
+    columns_count = len(matrix[0])
+    neighbors = 0
+    for y_gap in range(-1, 2):
+        for x_gap in range(-1, 2):
+            new_y = y_gap + y
+            new_x = x_gap + x
+            if new_y >= 0 and new_y < rows_count and \
+               new_x >= 0 and new_x < columns_count:
+                neighbors += matrix[new_y][new_x]
+    return neighbors - matrix[y][x]
 
 
 
-def nextGeneration(T):
-    tempT = creerTNull(len(T), len(T[0]))
-    for i in range(1, len(T)-1):
-        for j in range(1, len(T[i])-1):
-            nb = nbVoisins(T, i, j)
-            if nb < 2 or nb > 3: tempT[i][j] = 0
-            elif nb == 2: tempT[i][j] = T[i][j]
-            elif nb == 3: tempT[i][j] = 1
-    return tempT
+def next_generation(matrix):
+    rows_count = len(matrix)
+    columns_count = len(matrix[0])
+    fake_matrix = init_matrix(rows_count, columns_count)
+    for y in range(rows_count):
+        for x in range(columns_count):
+            new_state = None
+            neighbors = count_neighbors(matrix, y, x)
+            if neighbors < 2 or neighbors > 3: new_state = 0
+            elif neighbors == 2: new_state = matrix[y][x]
+            elif neighbors == 3: new_state = 1
+            fake_matrix[y][x] = new_state
+    return fake_matrix
 
 
 
-def afficherT(T):
-    effacer()
-    for i in T:
-        for j in i:
-            print("█", end="") if j ==1 else print(" ", end="")
+def draw_matrix(matrix):
+    clear_screen()
+    for rows in matrix:
+        for cell in rows:
+            print("█" if cell == 1 else " ", end = "")
         print()
 
 
 
-def effacer():
+def clear_screen():
     if name == "nt": system("cls")
     else: system("clear")
 
 
     
-def jeuDeLaVie(l, c):
-    T = creerTNull(l, c)
+def start_game(rows_count, columns_count):
+    matrix = init_matrix(rows_count, columns_count)
     while True:
-        T = nextGeneration(T)
-        afficherT(T)
-        sleep(.001)
+        matrix = next_generation(matrix)
+        draw_matrix(matrix)
+        sleep(.1)
 
 
 
-jeuDeLaVie(30, 100)
+start_game(30, 30)
